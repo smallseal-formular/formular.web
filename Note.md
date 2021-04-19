90aeed963e01b9049231f99dff9d290f5960cb20
shengwei0515 <small_seal@ShaneSyu.local>
2021-04-20 01:36:26 +0800

    feat: a demo to use iview table

---
+++ Note.md
@@ -0,0 +1,9 @@
+# 開發筆記
+
+# Init Project
+* vue create formular.web
+* install viewUI and boostrap
+    1. npm install plugin
+       npm install bootstrap-vue 
+       npm install view-design
+    2. import view-design and boostrap in main.ts
---
+++ formular.web/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <Table :columns="columns" :data="datas"></Table>
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({ 
+  components: {
+  },
+})
+export default class Home extends Vue {
+  data() {
+    return {
+      columns:[
+        {
+          title: 'Name',
+          key: 'name'
+        },
+        {
+          title: "Value",
+          key: 'value'
+        }
+      ],
+      datas: [
+        { 
+          name: 'TestName',
+          value: 'TestValue'
+        }
+      ]
+    }
+  }
+}
+</script>
