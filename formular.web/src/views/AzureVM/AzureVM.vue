4afab6bf4afa92ce4e5d7dc5baf8763de7bf0358
shengwei0515 <small_seal@ShaneSyu.local>
2021-05-24 02:43:23 +0800

    feat: finish vm detail modal

--- formular.web/src/views/AzureVM/AzureVM.vue
+++ formular.web/src/views/AzureVM/AzureVM.vue
@@ -2,14 +2,43 @@
   <div class="vm-list">
     <v-client-table :data="tableData" :columns="columns" :options="options">
       <div slot="vmDetailButton" slot-scope="props">
-          <Button @click="onClickDetailButton(props.row.vmDetailButton)">{{props.row.vmDetailButton}}</Button>
+          <Button @click.stop="onClickDetailButton(props.row.resourceGroupName)"></Button>
       </div>
-        <!-- <Button @click="modal2 = true" div slot="detroyButton" slot-scope="props" v-model="props.row.destoryButton">Destroy</Button> -->
+      <div slot="detroyButton" slot-scope="props">
+          <Button @click.stop="onClickDestroyButton(props.row.terraformWorkspace)"></Button>
+      </div>
     </v-client-table>
-    <Modal v-model="isShowDetailModal" title="Standard" 
-          @on-ok="DetailModalOk" @on-cancel="DetailModalCancel" 
+    <Modal v-model="isShowDetailModal" title="VmDetail">
+        <Collapse>
+          <Panel v-for="vm in vmDetailData"  :key='vm'>
+            {{ vm.vmName }}
+            <p slot="content">VM Size: {{ vm.vmSize }}</p>
+            <p slot="content">Public IP: {{ vm.publicIp }}</p>
+            <p slot="content">Private IP: {{ vm.privateIp }}</p>
+          </Panel>
+        </Collapse>
+        <div slot="footer">
+            <Button type="OK" size="large" @click="detailModalOk">OK</Button>
+        </div>
+        <!-- <li v-for="vm in vmDetailData"  :key='vm' >{{vm}}</li> -->
+    </Modal>
+    <Modal v-model="isShowDestroyModal" title="Destroy" 
+          @on-ok="destroyModalOk" @on-cancel="destroyModalCancel" 
           ok-text="Ok" cancel-text="Cancel">
-        <p>{{vmDetailInfo}}</p>
+        <Form :model="destroyFrom">
+          <FormItem label="TerraformWorkspace">
+            <Input v-model="destroyFrom.terraformWorkspace" placeholder="The ENV_NAME to destroy this environment by jenkins"></Input>
+          </FormItem>
+          <FormItem label="JenkinServerName">
+            <Input :disabled="true" v-model="destroyFrom.jenkinsServerName" placeholder="Which jenkins server you want to send the api"></Input>
+          </FormItem>
+          <FormItem label="JenkinsJobName">
+            <Input :disabled="true" v-model="destroyFrom.jenkinsJobName" placeholder="Which jenkins job is used to destroy vm"></Input>
+          </FormItem>
+          <FormItem label="JenkinsJobBranch">
+            <Input :disabled="true" v-model="destroyFrom.jenkinsJobBranch" placeholder="Which job branch is used to destroy vm"></Input>
+          </FormItem>
+        </Form>
     </Modal>
   </div>
 </template>
@@ -32,11 +61,20 @@ export default Vue.extend({
   data() {
     return {
       isShowDetailModal: false,
+      isShowDestroyModal: false,
       vmDetailInfo: {},
       columns: ["createAt", "terraformWorkspace", "resourceGroupName", "jobTrigger","vmDetailButton", "detroyButton"],
       tableData: [],
       options: {
-      }
+      },
+      vmDetailData: [],
+      destroyFrom: {
+        terraformWorkspace: "",
+        jenkinsServerName: "eventhub",
+        jenkinsJobName: "Azure.Machine",
+        jenkinsJobBranch: "develop",
+      },
+      destroyResponse: {},
     }
   },
   async mounted() {
@@ -44,26 +82,53 @@ export default Vue.extend({
                .then((res) => { this.tableData = res.data })
                .catch((error) => { this.$Message.error(error) })
                .finally(() => { /* 不論失敗成功皆會執行 */ });
-    console.log(this.tableData);
+    // console.log(this.tableData);
     // await axios.get('/api/azure/vm/table/getall')
     //            .then((res) => { this.tableData = res.data })
     //            .catch((error) => { console.error(error) })
     //            .finally();
   },
   methods: {
-    onClickDetailButton(detailName: string){
+    onClickDetailButton(rgName: string){
       this.isShowDetailModal = true;
-      this.vmDetailInfo = {"Detail":detailName}
-      this.$Message.info(`Clicked button, vmDetailInfo: ${JSON.stringify(this.vmDetailInfo)}`);
+      this.getVmDetailByRgName(rgName)
+      // this.$Message.info(`Clicked button, vmDetailData: ${JSON.stringify(this.vmDetailData)}`);
     },
-    DetailModalOk() {
-      this.vmDetailInfo = {}
-      this.$Message.info(`Clicked ok, vmDetailInfo: ${JSON.stringify(this.vmDetailInfo)}`);
+    async getVmDetailByRgName(rgName: string){ 
+      await axios.get(`/api/azure/vm/details/get/${rgName}`)
+                 .then((res) => { this.vmDetailData = res.data })
+                 .catch((error) => { this.$Message.error(error) })
+                 .finally(() => { /* 不論失敗成功皆會執行 */ });
     },
-    DetailModalCancel () {
+    detailModalOk() {
+      this.vmDetailData = []
+      this.isShowDetailModal = false;
+      // this.$Message.info(`Clicked ok, vmDetailInfo: ${JSON.stringify(this.vmDetailInfo)}`);
+    },
+    onClickDestroyButton(terraformWorkspace: string){
+      this.isShowDestroyModal = true;
+      this.destroyFrom.terraformWorkspace = terraformWorkspace
+      // this.$Message.info(`Clicked button, destroyMedal: ${JSON.stringify(this.vmDetailInfo)}`);
+    },
+    destroyModalOk() {
+      this.destroyVmByJenknisApi()
+      this.destroyFrom.terraformWorkspace = ""
+      this.isShowDestroyModal = false;
+      // this.$Message.info(`Clicked ok, vmDetailInfo: ${JSON.stringify(this.destroyFrom)}`);
+    },
+    destroyModalCancel () {
       this.vmDetailInfo = {}
-      this.$Message.info(`Clicked cancel, vmDetailInfo: ${JSON.stringify(this.vmDetailInfo)}`);
-    }
+      this.destroyFrom.terraformWorkspace = ""
+      this.isShowDestroyModal = false;
+      // this.$Message.info(`Clicked cancel, vmDetailInfo: ${JSON.stringify(this.isShowDestroyModal)}`);
+    },
+    async destroyVmByJenknisApi(){ 
+      await axios.post('/api/azure/vm/destroy/terraform-workspace', this.destroyFrom)
+                 .then( response =>  {
+                     this.$Message.info(`Send request to jenkins with status: ${response.statusText}`);
+                   }).catch((error) => { this.$Message.error(error) })
+                   .finally(() => { /* 不論失敗成功皆會執行 */ });
+    },
   }
 })
 
